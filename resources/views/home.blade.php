9e2bad9f761b8ad10d73d7100820e0dff0ff55d9
Santiago <santiago.mypantalla1@gmail.com>
2024-10-28 09:20:25 -0500

    se le agrega un favicon esta casi que completamente funcional todo, solo resta el sistema de regsitro por camara, ademas de manejo de horarios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
-    <title>Reloj MyPantalla</title>
+    <title>Intranet MyPantalla</title>
 </head>
 
 <body class="body">
